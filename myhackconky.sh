0e02d91ba92370283ee7664a82357549a037e64b
Anagnostakis Ioannis <412135+rizitis@users.noreply.github.com>
2023-02-04 13:39:18 +0200

    Update myhackconky.sh

--- myhackconky.sh
+++ myhackconky.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
- ./conky -q -c mainconky
- ./conky -q -c infofile
- ./conky -q -c rssfile
- ./conky -q -c topfile
+
+cd $(dirname $0)
+#killall conky
+ ./conky -q -c conkyrc &
+ sleep 1
+ ./conky -q -c topfile &
+ sleep 1
+ ./conky -q -c infofile &
+ sleep 1
+sed -i '/^$/d' rssfile
+./conky -q -c rssfile &
+ 
+ while true
+do 
+    echo "" >> rssfile
+    sleep 300
+done & 
+    
